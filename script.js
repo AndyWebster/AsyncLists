5e356b3c8427ef9b081ac2852ac3271b96516cbe
AndyWebster <andy.webster.online@gmail.com>
2020-03-25 12:07:33 +0000

    Implemented filtering

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// MOCK API FOR TESTING
+// ---------------------------------------------
 const pageOneData = {
     pageCount: 2,
     page: 1,
@@ -226,7 +228,6 @@ const pageTwoData = {
         ],
     ]
 }
-
 async function getData(page = 1) {
     function sendResponse() {
         if (page === 1) {
@@ -243,18 +244,18 @@ async function getData(page = 1) {
     });
 
 }
+// ---------------------------------------------
 
-// Get dom data
-const parent = document.querySelector('[async-endpoint]')
-const endpoint = parent.getAttribute('async-endpoint')
-const templateSelector = parent.getAttribute('template-selector')
+// HELPERS
+function isFunction(functionToCheck) {
+    return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+}
 
-// DEFINE STATE MANAGEMENT
+
+// DEFINE STATE HOOK
 function useState(args) {
     const { getter, setter, initialValue } = args
-    function isFunction(functionToCheck) {
-        return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
-    }
+
     function setState(fn) {
         const prev = getter()
         const next = isFunction(fn) ? fn(prev) : fn
@@ -267,6 +268,38 @@ function useState(args) {
     ]
 }
 
+// DEFINE DEBOUNCE
+function initThrottle(t, callback) {
+    let isActive;
+    let timer;
+
+    function stop() {
+        timer && clearTimeout(timer);
+        isActive = false;
+        callback && isFunction(callback) && callback()
+    }
+
+    function start() {
+        if (isActive) {
+            return
+        }
+        isActive = true;
+        timer = setTimeout(stop, t*1000)
+    }
+  
+    return ({
+        get active() {
+            return isActive;
+        },
+        start,
+        stop
+    })
+}
+
+// GET PARENT AND ENDPOINT
+const parent = document.querySelector('[async-endpoint]')
+const endpoint = parent.getAttribute('async-endpoint')
+
 // INIT STATE MANAGEMENT
 const [ getLoading, setLoading ] = useState({
     getter: () => parent.getAttribute('async-loading'),
@@ -278,65 +311,130 @@ const [ getPage, setPage ] = useState({
     setter: value =>  parent.setAttribute('async-page', value),
     initialValue: 0
 })
-const [ getPageCount, setPageCount ] = useState({
-    getter: () => parent.getAttribute('async-page-total'),
-    setter: value =>  parent.setAttribute('async-page-total', value),
-    initialValue: 1
-})
 
-function cloneAndFill(item) {
-    const clone = document.querySelector(templateSelector).cloneNode(true)
-    const newNode = parent.appendChild(clone)
-    newNode.removeAttribute('hidden')
-    newNode.setAttribute('template-clone', true)
-    item.forEach(property => {
-        const { selector, type, value, attribute } = property
-        const target = newNode.querySelector(selector)
+// FILTERS
+function getFilterArray() {
+    const filterSelector = parent.getAttribute('filter-selector')
+    const filterList = document.querySelectorAll(filterSelector)
+   return [...filterList]
+}
+function getActiveFilters() {
+    function isChecked(checkbox) {
+        return checkbox.checked
+    }
+    function getName(element) {
+        return element.name
+    }
+    return  getFilterArray().filter(isChecked).map(getName)
+}
+function initFilters(makeRequest) {
+    try {
+        function setDelay(callback, t) {
+            let hasChanged = false
+            let timerId = null
+        
+            function newTimer(fn) {
+                hasChanged = true
+                timerId = setTimeout( () => {
+                    hasChanged = false
+                    fn()
+                },t)
+            }
+            function start(fn) {
+                newTimer(fn)
+            }
+            function restart(fn) {
+                clearTimeout(timerId)
+                newTimer(fn)
+            }
+            function update(fn = callback) {
+                hasChanged ? restart(fn) : start(fn)
+            }
+            return update
+        }
 
-        // Handle different types of data
-        if (type === 'innerHTML') {
-            target.innerHTML = value
+        function handleChange() {
+            setPage(1)
+            makeRequest(getPage(), true)
         }
-        if (type === 'attribute') {
-            target.setAttribute(attribute, value)
+        
+        const handleChangeWithDelay = setDelay(handleChange, 1000)
+
+        function initFilterInput(element) {
+            element.addEventListener('change',() => handleChangeWithDelay())
         }
-    })
+
+        getFilterArray().forEach(initFilterInput)
+    }
+    catch (err) {
+        console.error(err)
+    }
 }
 
 
-
+// INIT PAGE CONTROLS
 const prev = document.querySelector('[async-prev]')
 const next = document.querySelector('[async-next]')
 const loadMore = document.querySelector('[async-more]')
-function checkButtonDisability(page, pageCount) {
-    if (prev && next) {
-        prev.disabled = (page <= 1)
-        next.disabled = (page >= pageCount)
-    }
-    if (loadMore) {
-        loadMore.disabled = (page === pageCount)
-    }
-}
 
 async function handleRequest(nextPage, clearEntries = false) {
+
+    function hydrateTemplate(item) {
+        const templateSelector = parent.getAttribute('template-selector')
+        const clone = document.querySelector(templateSelector).cloneNode(true)
+        const newNode = parent.appendChild(clone)
+        newNode.removeAttribute('hidden')
+        newNode.setAttribute('template-clone', true)
+        item.forEach(property => {
+            const { selector, type, value, attribute } = property
+            const target = newNode.querySelector(selector)
+
+            // Handle different types of data
+            if (type === 'innerHTML') {
+                target.innerHTML = value
+            }
+            if (type === 'attribute') {
+                target.setAttribute(attribute, value)
+            }
+        })
+    }
+
+    function updateButtonDisability(page, pageCount) {
+        if (prev && next) {
+            prev.disabled = (page <= 1)
+            next.disabled = (page >= pageCount)
+        }
+        if (loadMore) {
+            loadMore.disabled = (page === pageCount)
+        }
+    }
+
+    function deleteClones() {
+        const templateClones = document.querySelectorAll('[template-clone]')
+        if (templateClones && templateClones.length) {
+            const cloneArray = [...templateClones]
+            cloneArray.forEach(child => parent.removeChild(child))
+        }
+    }
+
     try {
         setLoading(true)
 
-        // FETCH RESPONSE
+        // FETCH DATA
+        const url = `${endpoint}?page=${nextPage}${["",...getActiveFilters()].join('&tag=')}`
+        const tempApiTarget = document.querySelector('[temp-api-target]')
+        tempApiTarget.innerText = url
+
         const response = await getData(nextPage)
         const { pageCount, page, content } = response
 
         // UPDATE STATE
-        checkButtonDisability(page, pageCount)
+        updateButtonDisability(page, pageCount)
         setPage(page)
-        setPageCount(pageCount)
-        
 
         // UPDATE DOM
-        if (clearEntries) {
-            parent.innerHTML = ""
-        }
-        content.forEach(cloneAndFill)
+        clearEntries && deleteClones()
+        content.forEach(hydrateTemplate)
         setLoading(false)
     } catch (err) {
         console.error(err)
@@ -346,32 +444,36 @@ async function handleRequest(nextPage, clearEntries = false) {
     
 }
 
-if (prev && next) {
+initFilters(handleRequest)
 
+if (prev && next) {
     function handlePrev() {
         const prevPage = getPage() - 1
         handleRequest(prevPage, true)
     }
-
     function handleNext() {
         const nextPage = getPage() + 1
         handleRequest(nextPage, true)
     }
 
+    // INITIAL LOAD
     handleNext()
+
+    // EVENT LISTENERS
     prev.addEventListener('click', handlePrev)
     next.addEventListener('click', handleNext)
 
 } else if (loadMore) {
-
     function handleMore() {
         const nextPage = getPage() + 1
         handleRequest(nextPage)
     }
 
+    // INITIAL LOAD
     handleMore()
+
+    // EVENT LISTENER
     loadMore.addEventListener('click', handleMore)
-    
 }
 
 
